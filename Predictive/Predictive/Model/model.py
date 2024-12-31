368a49f4ec1b825ea091a2e5e6b2bb473399c94f
Shardul <89907120+Shardul-1510@users.noreply.github.com>
2024-12-31 14:57:31 +0530

    Update model.py

--- Predictive/Predictive/Model/model.py
+++ Predictive/Predictive/Model/model.py
@@ -30,8 +30,8 @@ firm_collection = firm_db['financial_data']
 profit_collection = firm_db['firm_prediction']
 
 # Email configuration
-app.config['MAIL_SERVER'] = "smtp.gmail.com"
-app.config['MAIL_PORT'] = 587
+app.config['MAIL_SERVER'] = "smtp-server"
+app.config['MAIL_PORT'] = smtp-port number
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = "your-mailid"
 app.config['MAIL_PASSWORD'] = "your password"
